e2a5bc1d143790a1e5f885022df2242c2275b1fb
Kevin Campusano <kcampusano@endpointdev.com>
2022-05-30 15:25:07 -0400

    Add migration to fix datetime column snafu.
    Mentioned in c4df6152ef6bafa6fb331e620cf13524fb6c4125

---
+++ VehicleQuotes/Migrations/20220530192346_FixDatetimeColumn.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VehicleQuotes.Migrations
+{
+    public partial class FixDatetimeColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "quotes",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "created_at",
+                table: "quotes",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
