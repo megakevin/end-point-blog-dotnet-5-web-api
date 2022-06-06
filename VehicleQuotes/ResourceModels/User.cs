ce67cddf42bf7725ced0f92e98ae63449b4da539
Kevin Campusano <kcampusano@endpointdev.com>
2022-06-05 20:20:38 -0400

    Add new user resource model.
    To serve as request and response payload for user managemetnt endpoints.

---
+++ VehicleQuotes/ResourceModels/User.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace VehicleQuotes.ResourceModels
+{
+    public class User
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+    }
+}
