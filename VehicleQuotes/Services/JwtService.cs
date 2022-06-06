226fcc59ce38e4501afd63081bca99064af7f271
Kevin Campusano <kcampusano@endpointdev.com>
2022-06-06 19:21:04 -0400

    Add class to create JWTs: JwtService.

---
+++ VehicleQuotes/Services/JwtService.cs
@@ -0,0 +1,68 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using VehicleQuotes.ResourceModels;
+
+namespace VehicleQuotes.Services
+{
+    public class JwtService
+    {
+        private const int EXPIRATION_MINUTES = 1;
+
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public AuthenticationResponse CreateToken(IdentityUser user)
+        {
+            var expiration = DateTime.UtcNow.AddMinutes(EXPIRATION_MINUTES);
+
+            var token = CreateJwtToken(
+                CreateClaims(user),
+                CreateSigningCredentials(),
+                expiration
+            );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            return new AuthenticationResponse {
+                Token = tokenHandler.WriteToken(token),
+                Expiration = expiration
+            };
+        }
+
+        private JwtSecurityToken CreateJwtToken(Claim[] claims, SigningCredentials credentials, DateTime expiration) =>
+            new (
+                _configuration["Jwt:Issuer"],
+                _configuration["Jwt:Audience"],
+                claims,
+                expires: expiration,
+                signingCredentials: credentials
+            );
+
+        private Claim[] CreateClaims(IdentityUser user) =>
+            new[] {
+                new Claim(JwtRegisteredClaimNames.Sub, _configuration["Jwt:Subject"]),
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email)
+            };
+
+        private SigningCredentials CreateSigningCredentials() =>
+            new (
+                new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(_configuration["Jwt:Key"])
+                ),
+                SecurityAlgorithms.HmacSha256
+            );
+    }
+}
