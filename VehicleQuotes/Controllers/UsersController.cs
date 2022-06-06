0d080dbd91f7bb2639b4190cd99e9a07113aebe1
Kevin Campusano <kcampusano@endpointdev.com>
2022-06-06 19:22:06 -0400

    Add new endpoint for creating JWTs.

--- VehicleQuotes/Controllers/UsersController.cs
+++ VehicleQuotes/Controllers/UsersController.cs
@@ -2,6 +2,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using VehicleQuotes.ResourceModels;
+using VehicleQuotes.Services;
 
 namespace VehicleQuotes.Controllers
 {
@@ -10,11 +11,14 @@ namespace VehicleQuotes.Controllers
     public class UsersController : ControllerBase
     {
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly JwtService _jwtService;
 
         public UsersController(
-            UserManager<IdentityUser> userManager
+            UserManager<IdentityUser> userManager,
+            JwtService jwtService
         ) {
             _userManager = userManager;
+            _jwtService = jwtService;
         }
 
         [HttpPost]
@@ -55,6 +59,34 @@ namespace VehicleQuotes.Controllers
                 UserName = user.UserName,
                 Email = user.Email
             };
+        }
+
+        // POST: api/Users/BearerToken
+        [HttpPost("BearerToken")]
+        public async Task<ActionResult<AuthenticationResponse>> CreateBearerToken(AuthenticationRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var user = await _userManager.FindByNameAsync(request.UserName);
+
+            if (user == null)
+            {
+                return BadRequest("Bad credentials");
+            }
+
+            var isPasswordValid = await _userManager.CheckPasswordAsync(user, request.Password);
+
+            if (!isPasswordValid)
+            {
+                return BadRequest("Bad credentials");
+            }
+
+            var token = _jwtService.CreateToken(user);
+
+            return Ok(token);
         }
     }
 }
