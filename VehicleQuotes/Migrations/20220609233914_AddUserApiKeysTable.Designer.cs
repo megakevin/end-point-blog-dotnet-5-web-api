4d2fb29e1ade398ecb3b6c4df0793db3138efb09
Kevin Campusano <kcampusano@endpointdev.com>
2022-06-10 18:21:18 -0400

    Add migration for api keys table.

--- VehicleQuotes/Migrations/20220609233914_AddUserApiKeysTable.Designer.cs
+++ VehicleQuotes/Migrations/20220609233914_AddUserApiKeysTable.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using VehicleQuotes;
@@ -11,9 +12,10 @@ using VehicleQuotes;
 namespace VehicleQuotes.Migrations
 {
     [DbContext(typeof(VehicleQuotesContext))]
-    partial class VehicleQuotesContextModelSnapshot : ModelSnapshot
+    [Migration("20220609233914_AddUserApiKeysTable")]
+    partial class AddUserApiKeysTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VehicleQuotes/Migrations/20220609233914_AddUserApiKeysTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace VehicleQuotes.Migrations
+{
+    public partial class AddUserApiKeysTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "user_api_keys",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    value = table.Column<string>(type: "text", nullable: false),
+                    user_id = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_user_api_keys", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_user_api_keys_users_user_id",
+                        column: x => x.user_id,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_api_keys_user_id",
+                table: "user_api_keys",
+                column: "user_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_user_api_keys_value",
+                table: "user_api_keys",
+                column: "value",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user_api_keys");
+        }
+    }
+}
