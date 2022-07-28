2941f4d22208d2896bfe77802aabda1219d2e0db
Kevin Campusano <kcampusano@endpointdev.com>
2022-07-28 14:25:52 -0400

    Configure dependency injection and run user creator passing it the command line arguments.

--- VehicleQuotes.CreateUser/Program.cs
+++ VehicleQuotes.CreateUser/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using VehicleQuotes.CreateUser;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        var startup = new VehicleQuotes.Startup(context.Configuration);
+        startup.ConfigureServices(services);
+
+        services.AddTransient<UserCreator>();
+    })
+    .Build();
+
+var userCreator = host.Services.GetRequiredService<UserCreator>();
+userCreator.Run(args[0], args[1], args[2]);
+
